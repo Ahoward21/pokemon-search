9cb11184134ca99cc287fc2a1927c2767fa4e5d9
Kakashi <nickperel@live.com>
2021-12-02 23:16:53 -0500

    initial feature branch setup

--- index.js
+++ index.js
@@ -5,31 +5,25 @@ var nameInputEl = document.querySelector('#character');
 var characterContainerEl = document.querySelector('#character-container');
 var pokemonSearchTerm = document.querySelector('#pokemon-search');
 var pokemonSearch = "";
-
-
+var tradingCard = document.querySelector('#trading-card-container')
 /// -------------
 var formSubmitHandler = function (event) {
     // prevent page from refreshing
     event.preventDefault();
-
     // get value from input element
     pokemonSearch = nameInputEl.value.trim();
-
     if (pokemonSearch) {
         getPokemon(pokemonSearch);
-
         // clear old content
         characterContainerEl.textContent = '';
         nameInputEl.value = '';
     } else {
-        alert('Please enter pokemon');
+        // alert('Please enter pokemon');
     }
 };
 ///// ---------------------------------------------------------------
-
 var getPokemon = function (pokemon) {
     var pokeApiUrl = 'https://pokeapi.co/api/v2/pokemon/' + pokemon + "/";
-
     fetch(pokeApiUrl)
         .then(function (response) {
             // request was successful
@@ -39,7 +33,6 @@ var getPokemon = function (pokemon) {
                     console.log(data);
                     displayPokemon(data, pokemon);
                 });
-
             } else {
                 alert('Error: ' + response.statusText);
             }
@@ -47,35 +40,21 @@ var getPokemon = function (pokemon) {
         .catch(function (error) {
             console.log('Unable to connect to to pokemon');
         });
-
 };
-
 var displayPokemon = function (poke, searchTerm) {
     var pokemonName = document.createElement('span');
     pokemonName.textContent = (searchTerm)
     characterContainerEl.appendChild(pokemonName)
-
     for (var i = 0; i < poke.stats.length; i++) {
-        
         var statsEl = document.createElement('div');
-        
         statsEl.textContent = `${poke.stats[i].stat.name}: ${poke.stats[i].base_stat}`;
-
-        
-         
-         characterContainerEl.appendChild(statsEl)
-
-        
+        characterContainerEl.appendChild(statsEl)
     }
     pokeCard()
 }
-
-
 //// -------------------------------------------------------------
-
 var pokeCard = function (cards) {
     var cardApi = 'https://api.pokemontcg.io/v2/cards?q=name:' + pokemonSearch;
-
     fetch(cardApi)
         .then(function (response) {
             // request was successful
@@ -94,20 +73,17 @@ var pokeCard = function (cards) {
             console.log('Unable to bring up pokemon card');
         });
 };
-
 var displayCard = function (cardImg) {
     //for (var i = 0; i < cardImg.length; i++) {
-      //  console.log("this ran")
-        //console.log(cardImg[i])
-
+    console.log("this ran")
+    console.log("card img", cardImg[0].images.large)
     // }
+    var pokeCardImg = document.createElement('img');
+    pokeCardImg.src = (cardImg[0].images.large);
+    tradingCard.appendChild(pokeCardImg);
     console.log(cardImg[0])
-
-
+    /// create a href for link to image
+    ///
 }
-
-
-
 // add event listeners to forms
 userFormEl.addEventListener('submit', formSubmitHandler);
-;
